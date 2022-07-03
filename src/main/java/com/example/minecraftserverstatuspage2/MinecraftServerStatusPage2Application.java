f7be812caa18747f73ccca8403d649e5af0b77aa
Tom Troyer <tom.troyer@gmail.com>
2022-07-02 21:44:43 -0400

    Initial commit

---
+++ src/main/java/com/example/minecraftserverstatuspage2/MinecraftServerStatusPage2Application.java
@@ -0,0 +1,13 @@
+package com.example.minecraftserverstatuspage2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinecraftServerStatusPage2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinecraftServerStatusPage2Application.class, args);
+	}
+
+}
