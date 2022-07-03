d61e0c2ff3e6c78cedd1d320bb3cb3d4ef624130
Tom Troyer <tom.troyer@gmail.com>
2022-07-02 21:49:08 -0400

    Adding pebble

---
+++ src/main/java/com/example/minecraftserverstatuspage2/controllers/Status.java
@@ -0,0 +1,12 @@
+package com.example.minecraftserverstatuspage2.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Status {
+    @GetMapping("/")
+    public String getStatus() {
+        return "status";
+    }
+}
