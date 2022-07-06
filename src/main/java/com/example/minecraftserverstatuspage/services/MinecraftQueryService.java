cb90b20126948594818ea58df0d3c363230d20ba
Tom Troyer <tom.troyer@gmail.com>
2022-07-05 20:28:21 -0400

    Cleaning up some code

--- src/main/java/com/example/minecraftserverstatuspage/services/MinecraftQueryService.java
+++ src/main/java/com/example/minecraftserverstatuspage/services/MinecraftQueryService.java
@@ -5,7 +5,6 @@ import com.example.minecraftserverstatuspage.models.ServerConfigStatus;
 import com.example.minecraftserverstatuspage.models.ServerStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -16,22 +15,9 @@ import java.util.*;
 
 
 @Service
-@CacheConfig(cacheNames = {"serverstatus"})
 public class MinecraftQueryService {
     Logger logger = LoggerFactory.getLogger(MinecraftQueryService.class);
     private Map<ServerConfig, ServerConfigStatus> cache;
-
-    @Deprecated
-    public ServerConfigStatus getServerStatus(ServerConfig serverConfig) {
-        WebClient client = WebClient.create("https://api.mcsrvstat.us");
-
-        Mono<ServerStatus> response = client.get()
-                .uri("/2/" + serverConfig.address(), StandardCharsets.UTF_8)
-                .retrieve()
-                .bodyToMono(ServerStatus.class);
-
-        return new ServerConfigStatus(serverConfig, response.block());
-    }
 
     public List<ServerConfigStatus> getServerStatus(List<ServerConfig> serverConfigList) {
         List<ServerConfigStatus> results = new ArrayList<>();
@@ -54,14 +40,6 @@ public class MinecraftQueryService {
         return results;
     }
 
-    private boolean cacheContainsKey(ServerConfig serverConfig) {
-        return this.cache.containsKey(serverConfig);
-    }
-
-    private ServerConfigStatus getFromCache(ServerConfig serverConfig) {
-        return cache.get(serverConfig);
-    }
-
     private Mono<ServerStatus> getServerStatusAsync(ServerConfig serverConfig) {
         WebClient client = WebClient.create("https://api.mcsrvstat.us");
 
@@ -69,6 +47,14 @@ public class MinecraftQueryService {
                 .uri("/2/" + serverConfig.address(), StandardCharsets.UTF_8)
                 .retrieve()
                 .bodyToMono(ServerStatus.class);
+    }
+
+    private boolean cacheContainsKey(ServerConfig serverConfig) {
+        return this.cache.containsKey(serverConfig);
+    }
+
+    private ServerConfigStatus getFromCache(ServerConfig serverConfig) {
+        return cache.get(serverConfig);
     }
 
     /**
